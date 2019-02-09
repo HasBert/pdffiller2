4e34f9ede2a81b343d8309b42e518a3c97ca9b25
Unknown <MarvinHasenmayer@me.com>
2019-02-09 18:24:46 +0100

    updated dependencies, started rewrite

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
-PDF Filler (Node.js)
-======
-[![NPM](https://nodei.co/npm/pdffiller.png?downloads=true&downloadRank=true&stars=true)](https://nodei.co/npm/pdffiller/)
+PDF Filler 2 (Node.js)
+Forked from [pdffillerjs/pdffiller](https://github.com/pdffillerjs/pdffiller)
+Currently in development!
+====
 
 A node.js PDF form field data filler and FDF generator toolkit. This essentially is a wrapper around the PDF Toolkit library <a target="_blank" href="http://www.pdflabs.com/tools/pdftk-the-pdf-toolkit/">PDF ToolKit</a>.
 
-
-Quick start
------------
+## Quick start
 
 First, run `npm install pdffiller --save` for your app.
 
@@ -18,32 +17,31 @@ var pdfFiller = require('pdffiller');
 // ...
 ```
 
-
 ## Examples
 
 #### 1.Fill PDF with existing FDF Data
-````javascript
-var pdfFiller   = require('pdffiller');
 
-var sourcePDF = "test/test.pdf";
-var destinationPDF =  "test/test_complete.pdf";
+```javascript
+var pdfFiller = require('pdffiller');
+
+var sourcePDF = 'test/test.pdf';
+var destinationPDF = 'test/test_complete.pdf';
 var data = {
-    "last_name" : "John",
-    "first_name" : "Doe",
-    "date" : "Jan 1, 2013",
-    "football" : "Off",
-    "baseball" : "Yes",
-    "basketball" : "Off",
-    "hockey" : "Yes",
-    "nascar" : "Off"
+  last_name: 'John',
+  first_name: 'Doe',
+  date: 'Jan 1, 2013',
+  football: 'Off',
+  baseball: 'Yes',
+  basketball: 'Off',
+  hockey: 'Yes',
+  nascar: 'Off'
 };
 
-pdfFiller.fillForm( sourcePDF, destinationPDF, data, function(err) {
-    if (err) throw err;
-    console.log("In callback (we're done).");
+pdfFiller.fillForm(sourcePDF, destinationPDF, data, function(err) {
+  if (err) throw err;
+  console.log("In callback (we're done).");
 });
-
-````
+```
 
 This will take the test.pdf, fill the fields with the data values
 and create a complete filled in PDF (test_filled_in.pdf). Note that the
@@ -51,38 +49,46 @@ resulting PDF will be read-only.
 
 Alternatively,
 
-````javascript
-
+```javascript
 var shouldFlatten = false;
 
-pdfFiller.fillFormWithFlatten( sourcePDF, destinationPDF, data, shouldFlatten, function(err) {
+pdfFiller.fillFormWithFlatten(
+  sourcePDF,
+  destinationPDF,
+  data,
+  shouldFlatten,
+  function(err) {
     if (err) throw err;
     console.log("In callback (we're done).");
-})
-````
+  }
+);
+```
 
 Calling
 `fillFormWithFlatten()` with `shouldFlatten = false` will leave any unmapped fields
 still editable, as per the `pdftk` command specification.
 
-
 #### 2. Generate FDF Template from PDF
-````javascript
-var pdfFiller   = require('pdffiller');
 
-var sourcePDF = "test/test.pdf";
+```javascript
+var pdfFiller = require('pdffiller');
+
+var sourcePDF = 'test/test.pdf';
 
 // Override the default field name regex. Default: /FieldName: ([^\n]*)/
-var nameRegex = null;  
+var nameRegex = null;
 
-var FDF_data = pdfFiller.generateFDFTemplate( sourcePDF, nameRegex, function(err, fdfData) {
-    if (err) throw err;
-    console.log(fdfData);
+var FDF_data = pdfFiller.generateFDFTemplate(sourcePDF, nameRegex, function(
+  err,
+  fdfData
+) {
+  if (err) throw err;
+  console.log(fdfData);
 });
-
-````
+```
 
 This will print out this
+
 ```
 {
     "last_name" : "",
@@ -97,68 +103,70 @@ This will print out this
 ```
 
 #### 3. Map form fields to PDF fields
-````javascript
+
+```javascript
 var pdfFiller = require('pdffiller');
 
 var convMap = {
-    "lastName": "last_name",
-    "firstName": "first_name",
-    "Date": "date",
-    "footballField": "football",
-    "baseballField": "baseball",
-    "bballField": "basketball",
-    "hockeyField": "hockey",
-    "nascarField": "nascar"
+  lastName: 'last_name',
+  firstName: 'first_name',
+  Date: 'date',
+  footballField: 'football',
+  baseballField: 'baseball',
+  bballField: 'basketball',
+  hockeyField: 'hockey',
+  nascarField: 'nascar'
 };
 
 var fieldJson = [
-    {
-        "title" : "lastName",
-        "fieldfieldType": "Text",
-        "fieldValue": "John"
-    },
-    {
-        "title" : "firstName",
-        "fieldfieldType": "Text",
-        "fieldValue": "Doe"
-    },
-    {
-        "title" : "Date",
-        "fieldType": "Text",
-        "fieldValue": "Jan 1, 2013"
-    },
-    {
-        "title" : "footballField",
-        "fieldType": "Button",
-        "fieldValue": false
-    },
-    {
-        "title" : "baseballField",
-        "fieldType": "Button",
-        "fieldValue": true
-    },
-    {
-        "title" : "bballField",
-        "fieldType": "Button",
-        "fieldValue": false
-    },
-    {
-        "title" : "hockeyField",
-        "fieldType": "Button",
-        "fieldValue": true
-    },
-    {
-        "title" : "nascarField",
-        "fieldType": "Button",
-        "fieldValue": false
-    }
+  {
+    title: 'lastName',
+    fieldfieldType: 'Text',
+    fieldValue: 'John'
+  },
+  {
+    title: 'firstName',
+    fieldfieldType: 'Text',
+    fieldValue: 'Doe'
+  },
+  {
+    title: 'Date',
+    fieldType: 'Text',
+    fieldValue: 'Jan 1, 2013'
+  },
+  {
+    title: 'footballField',
+    fieldType: 'Button',
+    fieldValue: false
+  },
+  {
+    title: 'baseballField',
+    fieldType: 'Button',
+    fieldValue: true
+  },
+  {
+    title: 'bballField',
+    fieldType: 'Button',
+    fieldValue: false
+  },
+  {
+    title: 'hockeyField',
+    fieldType: 'Button',
+    fieldValue: true
+  },
+  {
+    title: 'nascarField',
+    fieldType: 'Button',
+    fieldValue: false
+  }
 ];
 
-var mappedFields = pdfFiller.mapForm2PDF( fieldJson, convMap );
+var mappedFields = pdfFiller.mapForm2PDF(fieldJson, convMap);
 console.log(mappedFields);
-````
+```
 
 This will print out the object below.
+
 ```
 {
     "last_name" : "John",
@@ -173,7 +181,8 @@ This will print out the object below.
 ```
 
 #### 4. Convert fieldJson to FDF data
-````javascript
+
+```javascript
 var pdfFiller   = require('pdffiller');
 
 var fieldJson = [
@@ -221,10 +230,11 @@ var fieldJson = [
 
 var FDFData = pdfFiller.convFieldJson2FDF( fieldJson );
 console.log(FDFData)
-````
+```
 
 This will print out this
-````
+
+```
 {
     "last_name" : "John",
     "first_name" : "Doe",
@@ -235,4 +245,4 @@ This will print out this
     "hockey" : "Yes",
     "nascar" : "Off"
 };
-````
+```
--- index.js
+++ index.js
@@ -1,150 +1,195 @@
 /*
-*   File:       index.js (pdffiller)
-*   Project:    PDF Filler
-*   Date:       May 2015.
-*
-*   Description: This PDF filler module takes a data set and creates a filled out
-*                PDF file with the form fields populated.
-*/
-(function(){
-    var child_process = require('child_process'),
-        execFile = require('child_process').execFile,
-        fdf = require('utf8-fdf-generator'),
-        _ = require('lodash'),
-        fs = require('fs');
+ *   File:       index.js (pdffiller)
+ *   Project:    PDF Filler
+ *   Date:       February 2019.
+ *
+ *   Description: This PDF filler module takes a data set and creates a filled out
+ *                PDF file with the form fields populated.
+ */
+(function() {
+  const child_process = require('child_process');
+  const execFile = require('child_process').execFile;
+  const fdf = require('utf8-fdf-generator');
+  const _ = require('lodash');
+  const fs = require('fs');
 
-    var pdffiller = {
+  const pdffiller = {
+    mapForm2PDF: function(formFields, convMap) {
+      let tmpFDFData = this.convFieldJson2FDF(formFields);
+      tmpFDFData = _.mapKeys(tmpFDFData, function(value, key) {
+        try {
+          convMap[key];
+        } catch (err) {
+          return key;
+        }
+        return convMap[key];
+      });
 
-        mapForm2PDF: function( formFields, convMap ){
-            var tmpFDFData = this.convFieldJson2FDF(formFields);
-            tmpFDFData = _.mapKeys(tmpFDFData, function(value, key){
-                try {
-                    convMap[key];
-                } catch(err){
+      return tmpFDFData;
+    },
 
-                    return key;
-                }
-                return convMap[key];
-            });
+    convFieldJson2FDF: function(fieldJson) {
+      let _keys = _.map(fieldJson, 'title'),
+        _values = _.map(fieldJson, 'fieldValue');
 
-            return tmpFDFData;
-        },
+      _values = _.map(_values, function(val) {
+        return val === true ? 'Yes' : 'Off';
+      });
 
-        convFieldJson2FDF: function(fieldJson){
-            var _keys = _.pluck(fieldJson, 'title'),
-                _values = _.pluck(fieldJson, 'fieldValue');
+      let jsonObj = _.zipObject(_keys, _values);
 
-            _values = _.map(_values, function(val){
-                if(val === true){
-                    return 'Yes';
-                }else if(val === false) {
-                    return 'Off';
-                }
-                return val;
-            });
+      return jsonObj;
+    },
 
-            var jsonObj = _.zipObject(_keys, _values);
+    generateFieldJson: function(sourceFile, nameRegex, callback) {
+      let regName = /FieldName: ([^\n]*)/,
+        regType = /FieldType: ([A-Za-z\t .]+)/,
+        regFlags = /FieldFlags: ([0-9\t .]+)/,
+        regOptions = /FieldStateOption: ([A-Za-z0-9\t]+)/,
+        regValues = /FieldValue: ([^\n .]+)/,
+        fieldArray = [],
+        currField = {};
 
-            return jsonObj;
-        },
+      if (nameRegex !== null && typeof nameRegex == 'object')
+        regName = nameRegex;
 
-        generateFieldJson: function( sourceFile, nameRegex, callback){
-            var regName = /FieldName: ([^\n]*)/,
-                regType = /FieldType: ([A-Za-z\t .]+)/,
-                regFlags = /FieldFlags: ([0-9\t .]+)/,
-                fieldArray = [],
-                currField = {};
+      execFile('pdftk', [sourceFile, 'dump_data_fields_utf8'], function(
+        error,
+        stdout,
+        stderr
+      ) {
+        console.log(stdout);
 
-            if(nameRegex !== null && (typeof nameRegex) == 'object' ) regName = nameRegex;
-
-            execFile( "pdftk", [sourceFile, "dump_data_fields_utf8"], function (error, stdout, stderr) {
-                if (error) {
-                    console.log('exec error: ' + error);
-                    return callback(error, null);
-                }
-
-                fields = stdout.toString().split("---").slice(1);
-                fields.forEach(function(field){
-                    currField = {};
-
-                    currField['title'] = field.match(regName)[1].trim() || '';
-
-                    if(field.match(regType)){
-                        currField['fieldType'] = field.match(regType)[1].trim() || '';
-                    }else {
-                        currField['fieldType'] = '';
-                    }
-
-                    if(field.match(regFlags)){
-                        currField['fieldFlags'] = field.match(regFlags)[1].trim()|| '';
-                    }else{
-                        currField['fieldFlags'] = '';
-                    }
-
-                    currField['fieldValue'] = '';
-
-                    fieldArray.push(currField);
-                });
-
-                return callback(null, fieldArray);
-            });
-        },
-
-        generateFDFTemplate: function( sourceFile, nameRegex, callback ){
-            this.generateFieldJson(sourceFile, nameRegex, function(err, _form_fields){
-                if (err) {
-                  console.log('exec error: ' + err);
-                  return callback(err, null);
-                }
-
-                return callback(null, this.convFieldJson2FDF(_form_fields));
-
-            }.bind(this));
-        },
-
-        fillFormWithOptions: function( sourceFile, destinationFile, fieldValues, shouldFlatten, tempFDFPath, callback ) {
-
-
-            //Generate the data from the field values.
-            var randomSequence = Math.random().toString(36).substring(7);
-            var currentTime = new Date().getTime();
-            var tempFDFFile =  "temp_data" + currentTime + randomSequence + ".fdf",
-                tempFDF = (typeof tempFDFPath !== "undefined"? tempFDFPath + '/' + tempFDFFile: tempFDFFile),
-
-                formData = fdf.generator( fieldValues, tempFDF );
-
-            var args = [sourceFile, "fill_form", tempFDF, "output", destinationFile];
-            if (shouldFlatten) {
-                args.push("flatten");
-            }
-            execFile( "pdftk", args, function (error, stdout, stderr) {
-
-                if ( error ) {
-                    console.log('exec error: ' + error);
-                    return callback(error);
-                }
-                //Delete the temporary fdf file.
-                fs.unlink( tempFDF, function( err ) {
-
-                    if ( err ) {
-                        return callback(err);
-                    }
-                    // console.log( 'Sucessfully deleted temp file ' + tempFDF );
-                    return callback();
-                });
-            } );
-        },
-
-        fillFormWithFlatten: function( sourceFile, destinationFile, fieldValues, shouldFlatten, callback ) {
-            this.fillFormWithOptions( sourceFile, destinationFile, fieldValues, shouldFlatten, undefined, callback);
-        },
-
-        fillForm: function( sourceFile, destinationFile, fieldValues, callback) {
-            this.fillFormWithFlatten( sourceFile, destinationFile, fieldValues, true, callback);
+        if (error) {
+          console.log('exec error: ' + error);
+          return callback(error, null);
         }
 
-    };
+        fields = stdout
+          .toString()
+          .split('---')
+          .slice(1);
+        fields.forEach(function(field) {
+          currField = {};
 
-    module.exports = pdffiller;
+          currField['title'] = field.match(regName)[1].trim() || '';
 
-}())
+          if (field.match(regType)) {
+            currField['fieldType'] = field.match(regType)[1].trim() || '';
+          } else {
+            currField['fieldType'] = '';
+          }
+
+          if (field.match(regFlags)) {
+            currField['fieldFlags'] = field.match(regFlags)[1].trim() || '';
+          } else {
+            currField['fieldFlags'] = '';
+          }
+
+          currField['fieldOptions'] = [];
+          if (field.match(regOptions)) {
+            console.log(
+              'VALUES!!!' + JSON.stringify(field.match(regOptions)[1])
+            );
+            const fieldOption = field.match(regOptions)[1].trim();
+            currField['fieldOptions'].push(fieldOption);
+          }
+
+          if (field.match(regValues)) {
+            currField['fieldValue'] = field.match(regValues)[1].trim() || '';
+          } else {
+            currField['fieldValue'] = '';
+          }
+
+          fieldArray.push(currField);
+        });
+
+        return callback(null, fieldArray);
+      });
+    },
+
+    generateFDFTemplate: function(sourceFile, nameRegex, callback) {
+      this.generateFieldJson(
+        sourceFile,
+        nameRegex,
+        function(err, _form_fields) {
+          if (err) {
+            console.log('exec error: ' + err);
+            return callback(err, null);
+          }
+
+          return callback(null, this.convFieldJson2FDF(_form_fields));
+        }.bind(this)
+      );
+    },
+
+    fillFormWithOptions: function(
+      sourceFile,
+      destinationFile,
+      fieldValues,
+      shouldFlatten,
+      tempFDFPath,
+      callback
+    ) {
+      //Generate the data from the field values.
+      let randomSequence = Math.random()
+        .toString(36)
+        .substring(7);
+      let currentTime = new Date().getTime();
+      let tempFDFFile = 'temp_data' + currentTime + randomSequence + '.fdf',
+        tempFDF =
+          typeof tempFDFPath !== 'undefined'
+            ? tempFDFPath + '/' + tempFDFFile
+            : tempFDFFile,
+        formData = fdf.generator(fieldValues, tempFDF);
+
+      let args = [sourceFile, 'fill_form', tempFDF, 'output', destinationFile];
+      if (shouldFlatten) {
+        args.push('flatten');
+      }
+      execFile('pdftk', args, function(error, stdout, stderr) {
+        if (error) {
+          console.log('exec error: ' + error);
+          return callback(error);
+        }
+        //Delete the temporary fdf file.
+        fs.unlink(tempFDF, function(err) {
+          if (err) {
+            return callback(err);
+          }
+          // console.log( 'Sucessfully deleted temp file ' + tempFDF );
+          return callback();
+        });
+      });
+    },
+
+    fillFormWithFlatten: function(
+      sourceFile,
+      destinationFile,
+      fieldValues,
+      shouldFlatten,
+      callback
+    ) {
+      this.fillFormWithOptions(
+        sourceFile,
+        destinationFile,
+        fieldValues,
+        shouldFlatten,
+        undefined,
+        callback
+      );
+    },
+
+    fillForm: function(sourceFile, destinationFile, fieldValues, callback) {
+      this.fillFormWithFlatten(
+        sourceFile,
+        destinationFile,
+        fieldValues,
+        true,
+        callback
+      );
+    }
+  };
+
+  module.exports = pdffiller;
+})();
